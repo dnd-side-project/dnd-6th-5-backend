6e079ac63dc6f0107e6a0796ac5e1d3cf2c9add6
julia-ing <ywchoi0625@gmail.com>
2022-01-17 14:00:16 +0900

    style: add semicolon in app.ts

--- srcs/app.ts
+++ srcs/app.ts
@@ -1,6 +1,6 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-app.listen(process.env.PORT, () => console.log(`server port ${process.env.PORT}`))
+app.listen(process.env.PORT, () => console.log(`server port ${process.env.PORT}`));
