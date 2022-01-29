218da77b88d9073d43927242e9e113ee1fb195a5
julia-ing <ywchoi0625@gmail.com>
2022-01-30 01:27:59 +0900

    env: create User entity - common/Enums.ts: enum 정리

--- srcs/entity/User.ts
+++ srcs/entity/User.ts
@@ -1,12 +1,56 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    BaseEntity,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { CompanyScale, WorkPeriod, Income, Asset, HasHouse, IsHouseholder } from './common/Enums';
 
 @Entity()
-export class User {
+export class User extends BaseEntity {
     @PrimaryGeneratedColumn()
     id!: number;
 
-    @Column()
-    name!: string;
+    @Column('varchar', { name: 'nickname', unique: true, nullable: false, length: 50 })
+    nickname!: string;
+
+    @Column({ type: 'int', nullable: false })
+    age!: number;
+
+    @Column('varchar', { name: 'address', length: 50, nullable: false })
+    address!: string;
+
+    @Column({ type: 'enum', name: 'company_scale', nullable: false })
+    companyScale!: CompanyScale;
+
+    @Column({ type: 'enum', name: 'work_period', nullable: false })
+    workPeriod!: WorkPeriod;
+
+    @Column({ type: 'enum', name: 'income' })
+    income!: Income;
+
+    @Column({ type: 'enum', name: 'asset' })
+    asset!: Asset;
+
+    @Column({ type: 'enum', name: 'has_house' })
+    hasHouse!: HasHouse;
+
+    @Column({ type: 'enum', name: 'is_householder' })
+    isHouseholder!: IsHouseholder;
+
+    @CreateDateColumn({
+        type: 'timestamp',
+        name: 'created_at',
+    })
+    createdAt: Date | undefined;
+
+    @UpdateDateColumn({
+        type: 'timestamp',
+        name: 'updated_at',
+    })
+    updatedAt: Date | undefined;
 }
 
 export default User;
---
+++ srcs/entity/common/Enums.ts
@@ -0,0 +1,52 @@
+export enum CompanyScale {
+    NONE = '무직',
+    MS5 = '중소기업(5인 이하)',
+    MS30 = '중소기업(30인 이하)',
+    MS50 = '중소기업(50인 이하)',
+    MEDIUM = '중견기업',
+    LARGE = '대기업',
+    FREE = '프리랜서',
+    SELF = '자영업자',
+}
+
+export enum WorkPeriod {
+    NONE = '없음',
+    ONE = '~ 1년 미만',
+    THREE = '1년 이상 ~ 3년 미만',
+    FIVE = '3년 이상 ~ 5년 미만',
+    MORE = '5년 이상 ~',
+}
+
+export enum Income {
+    TWOFIVE = '~ 2,500만원 미만',
+    THREE = '2,600만원 이상 ~ 3,000만원 미만',
+    THREEFIVE = '3,100만원 이상 ~ 3,500만원 미만',
+    FOUR = '3,600만원 이상 ~ 4,000만원 미만',
+    FOURFIVE = '4,100만원 이상 ~ 4,500만원 미만',
+    MORE = '4,600만원 이상 ~',
+    PRIVATE = '미공개',
+}
+
+export enum Asset {
+    NONE = '없음',
+    FIVE = '~ 5천만원 미만',
+    ONE = '5천만원 이상 ~ 1억 미만',
+    ONEFIVE = '1억 이상 ~ 1억 5천 미만',
+    TWO = '1억 5천 이상 ~ 2억 미만',
+    TWOFIVE = '2억 이상 ~ 2억 5천 미만',
+    THREE = '2억 5천 이상 ~ 3억 미만',
+    MORE = '3억 이상 ~',
+    PRIVATE = '미공개',
+}
+
+export enum HasHouse {
+    NONE = '무주택자',
+    HAS = '유주택자',
+    PRIVATE = '미공개',
+}
+
+export enum IsHouseholder {
+    HOST = '세대주',
+    MEMBER = '세대구성원',
+    PRIVATE = '미공개',
+}
