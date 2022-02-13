8b6f960aad44123714e5bfbccdcade5f99800d4f
Lks9172 <ekdan9172@naver.com>
2022-02-14 02:56:33 +0900

    env: Data input 정책 신청사이트의 title 입력 code 작성
    - mysql package는 promise문법을 지원하지 않기때문 mysql -> mysql2/promise로 패키지 변경
    - puppeteer를 사용하여 신청사이트의 title값 수집
    - 콜백문법 사용 소스코드 -> await/async문법으로 변경

--- srcs/inputData.ts
+++ srcs/inputData.ts
@@ -1,74 +1,131 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const convert = require('xml-js');
-const request = require('request');
-const mysql = require('mysql');
+const axios = require('axios');
+const mysql = require('mysql2/promise');
+const puppeteer = require('puppeteer');
+
 require('dotenv').config();
 
 const OPEN_API_KEY = process.env.OPEN_API_KEY;
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
-
 const requestUrl = `https://www.youthcenter.go.kr/opi/empList.do?display=100&pageIndex=1&bizTycdSel=004003&openApiVlak=${OPEN_API_KEY}`;
 
-request.get(requestUrl, (err, res, body) => {
-    if (err) {
-        console.log(`err => ${err}`);
-    } else {
-        if (res.statusCode == 200) {
-            const xmlToJson = convert.xml2json(body, { compact: true, spaces: 4 });
-            const json = JSON.parse(xmlToJson); // json 객체로 파싱
-
-            const emp = json.empsInfo.emp;
-
-            // 정책번호(추가 필요): polyBizSecd
-            // 이름: polyBizSjnm, name / 정책번호: bizId, number / 소개: polyItcnCn, summary / 내용: sporCn, content / 나이: ageInfo, limit_age / 취업상태: empmSttsCn, work_status / 특화분야: splzRlmRqisCn, specialization
-            // 신청기관: cnsgNmor, operating_institute / 신청기간: rqutPrdCn, application_period / 신청절차: rqutProcCn, application_process / 심사발표: jdgnPresCn, announcement / 사이트링크: rqutUrla, application_site
-            emp.forEach((data) => {
-                // 중앙부처 정책만
-                if (data.polyBizTy._cdata === '중앙부처') {
-                    // console.log(data);
-
-                    connection.query(
-                        `SELECT count(*) FROM policy where name = "${data.polyBizSjnm._cdata}"`,
-                        (err, results) => {
-                            if (err) throw err;
-
-                            // 정책 이름이 같은 데이터가 존재하지 않는다면 데이터 삽입
-                            if (results[0]['count(*)'] == 0) {
-                                connection.query(
-                                    `INSERT INTO policy(name, number, summary, content, limit_age, work_status, specialization, operating_institute, application_period, application_process, announcement, application_site) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)`,
-                                    [
-                                        data.polyBizSjnm._cdata,
-                                        data.bizId._text,
-                                        data.polyItcnCn._cdata,
-                                        data.sporCn._cdata,
-                                        data.ageInfo._cdata,
-                                        data.empmSttsCn._cdata,
-                                        data.splzRlmRqisCn._cdata,
-                                        data.cnsgNmor._cdata,
-                                        data.rqutPrdCn._cdata,
-                                        data.rqutProcCn._cdata,
-                                        data.jdgnPresCn._cdata,
-                                        data.rqutUrla._cdata,
-                                    ],
-                                    (err, results) => {
-                                        if (err) throw err;
-                                        console.log('results: ', results);
-                                    }
-                                );
-                            } else {
-                                // 정책 이름이 같은 데이터가 존재한다면 넘어감
-                                console.log('same policy already in db');
-                            }
-                        }
-                    );
-                }
-            });
-        }
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+async function getTitle(page, url) {
+    try {
+        await page.goto(url);
+        await page.waitForSelector('head > title');
+        const result = await page.$eval('head > title', (e) => e.innerText);
+        return result;
+    } catch (e) {
+        return false;
     }
-});
+}
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+async function gotoPage(url) {
+    if (url.includes(`https://www.kosaf.go.kr/ko/login_sc.do?rtn=/ssoext/sso_gateway.jsp?ref`))
+        return '한국장학재단';
+    const browser = await puppeteer.launch({
+        headless: true,
+    });
+    const page = await browser.newPage();
+    const result = await getTitle(page, url);
+    await browser.close();
+    return result;
+}
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+function replaceToUrl(str) {
+    let result = '';
+    let flag = true;
+    for (const s of str) {
+        if (s === 'h') flag = false;
+        if (flag === false) result += s;
+        else continue;
+    }
+    return result;
+}
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+async function insertPolicy(connection, results, data) {
+    const title = await getPureTitle(data);
+    console.log(title);
+    if (results[0]['count(*)'] == 0) {
+        const result = await connection
+            .query(
+                `INSERT INTO policy(name, number, summary, content, limit_age, work_status, specialization, operating_institute, application_period, application_process, announcement, application_site) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)`,
+                [
+                    data.polyBizSjnm._cdata,
+                    data.bizId._text,
+                    data.polyItcnCn._cdata,
+                    data.sporCn._cdata,
+                    data.ageInfo._cdata,
+                    data.empmSttsCn._cdata,
+                    data.splzRlmRqisCn._cdata,
+                    data.cnsgNmor._cdata,
+                    data.rqutPrdCn._cdata,
+                    data.rqutProcCn._cdata,
+                    data.jdgnPresCn._cdata,
+                    title,
+                ]
+            )
+            .catch((e) => {
+                return false;
+            });
+        if (result === false) console.log('results: ', results);
+    } else {
+        // 정책 이름이 같은 데이터가 존재한다면 넘어감
+        console.log('same policy already in db');
+    }
+}
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+async function getPureTitle(data) {
+    const uri = data.rqutUrla._cdata.replace(/[^0-9a-zA-Z&.,/:_?=-]/g, '');
+    const pureUrl = replaceToUrl(uri);
+    let title = await gotoPage(pureUrl);
+    if (title === false) title = data.rqutUrla._cdata;
+    if (title.includes('본인인증')) return '-';
+    return title;
+}
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+async function tourData(connection, data) {
+    // 중앙부처 정책만
+    if (data.polyBizTy._cdata !== '중앙부처') return;
+    const results = await connection
+        .query(`SELECT count(*) FROM policy where name = "${data.polyBizSjnm._cdata}"`)
+        .catch((e) => {
+            return false;
+        });
+    if (results === false) return;
+    await insertPolicy(connection, results, data);
+}
+
+// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+async function main() {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+
+    const body = await axios
+        .get(requestUrl)
+        .then((res) => {
+            return res.data;
+        })
+        .catch((err) => {
+            return err;
+        });
+    const xmlToJson = await convert.xml2json(body, { compact: true, spaces: 4 });
+    const json = await JSON.parse(xmlToJson); // json 객체로 파싱
+    const emp = await json.empsInfo?.emp;
+    for (const data of emp) {
+        await tourData(connection, data);
+    }
+    console.log('finish');
+}
+
+main();
