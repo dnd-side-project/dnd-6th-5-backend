328667b0fec24bafee6f0e67aba13e3fc462d770
Lks9172 <ekdan9172@naver.com>
2022-02-18 01:31:38 +0900

    feat:  한 user를 DB에서 조회해 user의 정보를 반환하는 컨트롤러 구현

--- srcs/controllers/controller.getOneUser.ts
+++ srcs/controllers/controller.getOneUser.ts
@@ -3,9 +3,12 @@ import { findOneUserById } from '../repository/index';
 
 const getOneUser: RequestHandler = async (req, res) => {
     try {
+        const userId: string = req.params.id;
+        const user = await findOneUserById(userId);
+
         return res.status(200).json({
             success: true,
-            data: '1',
+            data: { user },
         });
     } catch (error: any) {
         res.status(400).json({
