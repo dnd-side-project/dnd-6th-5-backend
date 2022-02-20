bf458cf98e6cbf506c48109e3c53012e56084a34
Lks9172 <ekdan9172@naver.com>
2022-02-20 17:04:13 +0900

    feat: findOneUserComment함수 존재하지 않는 userId일 경우 에러처리

--- srcs/repository/repository.comment.ts
+++ srcs/repository/repository.comment.ts
@@ -3,9 +3,11 @@ import { Post, Comment, User } from '../entity/index';
 const findOneUserComment: (id: string) => Promise<Comment[]> = async (id) => {
     const userId = parseInt(id);
     if (isNaN(userId)) throw Error('Please enter a numeric character for the id value.');
+    const targetUser = await User.findOne({ id: userId });
+    if (targetUser === undefined) throw Error(`This user_id does not exist.`);
 
     const result = await Comment.createQueryBuilder('comment')
-        .select(['user_id', 'P.post_id', 'id as comment_id', 'content', 'P.title'])
+        .select(['user_id', 'P.post_id', 'id as comment_id', 'P.title', 'content'])
         .leftJoin(
             (qb) =>
                 qb.from(Post, 'post').select(['title', 'created_at']).addSelect('id', 'post_id'),
