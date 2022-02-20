fce773c27902c1a9d1e3de96f412cc7b4eb5e980
Lks9172 <ekdan9172@naver.com>
2022-02-20 15:45:04 +0900

    docs: User 엔티티에 대한 swagger 컴포넌트 스키마 추가 작성

--- srcs/swagger/schema/user.ts
+++ srcs/swagger/schema/user.ts
@@ -41,3 +41,71 @@
  *                  hasHouse:
  *                      type: string
  */
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          UserEntity:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: number
+ *                  nickname:
+ *                      type: string
+ *                  age:
+ *                      type: string
+ *                  workStatus:
+ *                      type: string
+ *                  companyScale:
+ *                      type: string
+ *                  medianIncome:
+ *                      type: string
+ *                  annualIncome:
+ *                      type: string
+ *                  asset:
+ *                      type: string
+ *                  hasHouse:
+ *                      type: string
+ *                  isHouseOwner:
+ *                      type: string
+ *                  maritalStatus:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *                  createdAt:
+ *                      type: string
+ *                  updatedAt:
+ *                      type: string
+ */
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Filter:
+ *              type: object
+ *              properties:
+ *                  category:
+ *                      type: string
+ *                  id:
+ *                      type: number
+ *                  age:
+ *                      type: string
+ *                  maritalStatus:
+ *                      type: string
+ *                  workStatus:
+ *                      type: string
+ *                  companyScale:
+ *                      type: string
+ *                  medianIncome:
+ *                      type: string
+ *                  annualIncome:
+ *                      type: string
+ *                  asset:
+ *                      type: string
+ *                  isHouseOwner:
+ *                      type: string
+ *                  hasHouse:
+ *                      type: string
+ */
