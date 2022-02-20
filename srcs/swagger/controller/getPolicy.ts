2266addf482cf0176a3f88ed3b076edff917394b
julia-ing <ywchoi0625@gmail.com>
2022-02-20 22:14:54 +0900

    docs: policy 관련 api 문서 작성 완료

--- srcs/swagger/controller/getPolicy.ts
+++ srcs/swagger/controller/getPolicy.ts
@@ -15,19 +15,27 @@
  *          parameters:
  *              - in: query
  *                name: category
+ *                description: 카테고리 선택 (전체 / 주거 / 금융)
  *                schema:
  *                    type: string
  *          responses:
  *              200:
- *                  description: success
+ *                  description: 정책 리스트를 성공적으로 조회하였을 경우 다음 결과가 반환됩니다.
  *                  content:
  *                      application/json:
  *                          schema:
- *                              type: array
- *                              items:
- *                                  $ref: "#/components/schemas/Policy"
- *              400:
- *                  description: No policy in DB
+ *                              type: object
+ *                              properties:
+ *                                success:
+ *                                  type: boolean
+ *                                data:
+ *                                  type: object
+ *                                  properties:
+ *                                      policy:
+ *                                          type: array
+ *                                          items:
+ *                                              oneOf:
+ *                                              - $ref: "#/components/schemas/Policy"
  */
 
 /**
@@ -44,22 +52,43 @@
  *                    type: number
  *          responses:
  *              200:
- *                  description: success
- *                  content:
- *                      application/json:
- *                          schema:
- *                              $ref: "#/components/schemas/PolicyDetail"
- *              404:
- *                  description: No matching policy in DB
+ *                  description: 특정 policy의 정보를 성공적으로 조회하였을 경우 다음 결과가 반환됩니다.
  *                  content:
  *                      application/json:
  *                          schema:
  *                              type: object
  *                              properties:
- *                                  success:
- *                                      type: string
- *                                  error:
- *                                      type: string
+ *                                success:
+ *                                  type: boolean
+ *                                data:
+ *                                  type: object
+ *                                  properties:
+ *                                      policy:
+ *                                          $ref: "#/components/schemas/PolicyDetail"
+ *              404:
+ *                  description: |
+ *                    오류가 발생해 특정 policy(정책)에 대한 데이터를 성공적으로 조회하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
+ *                    -오류 예시</br>
+ *                    - 존재하지 않는 pk가 들어온 경우 <br>
+ *                    - No matching policy in db <br>
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                success:
+ *                                  type: boolean
+ *                                error:
+ *                                  properties:
+ *                                   code:
+ *                                     type: string
+ *                                   message:
+ *                                     type: string
+ *                              example:
+ *                                success: false
+ *                                error:
+ *                                  code: Error
+ *                                  message: No matching policy in db
  */
 
 /**
