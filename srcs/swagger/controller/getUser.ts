d577d8ac5f88ed8186c89d40cd45b71476e4a329
Lks9172 <ekdan9172@naver.com>
2022-02-20 15:47:24 +0900

    docs: patch /user/nickname swagger api 문서 작성 완료
    - 해당 api는 특정 user의 nickname값을 변경합니다.

--- srcs/swagger/controller/getUser.ts
+++ srcs/swagger/controller/getUser.ts
@@ -1,8 +1,16 @@
 /**
  * @swagger
+ * tags:
+ *   name: User
+ *   description: 유저
+ */
+
+/**
+ * @swagger
  * /user:
  *  get:
  *      description: get all users
+ *      tags: [User]
  *      responses:
  *          200:
  *              description: success
@@ -23,6 +31,7 @@
  * /user/{userId}:
  *  get:
  *      description: get user same userId
+ *      tags: [User]
  *      responses:
  *          200:
  *              description: success
@@ -34,4 +43,105 @@
  *              description: No user in DB
  *          401:
  *              description: Authentication Error
+ */
+
+/**
+ * @swagger
+ * /user/nickname:
+ *  patch:
+ *      description: 특정 user의 nickname값을 변경합니다.
+ *      parameters:
+ *      - in: header
+ *        name: access_token
+ *        type: string
+ *        required: true
+ *        description: 로그인하면서 발급받은 access_token
+ *      - in: header
+ *        name: platform
+ *        type: string
+ *        required: true
+ *        description: 로그인하면서 발급받은 platform값
+ *      tags: [User]
+ *      requestBody:
+ *        required: true
+ *        description: |
+ *          ### 변경할 상태 정보입니다.</br>
+ *          - id - nickname을 변경할 user의 id값입니다.</br>
+ *          - nickname - 변경될 nickname값입니다.</br>
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                id:
+ *                  type: string
+ *                nickname:
+ *                  type: string
+ *              example:
+ *                "id": "2"
+ *                "nickname": "kiwi"
+ *      responses:
+ *          200:
+ *              description: 특정 user를 subject 랜덤 매칭 대기열에 성공적으로 추가하였을 경우 다음 결과가 반환됩니다.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                            success:
+ *                              type: boolean
+ *                            data:
+ *                              type: object
+ *                              properties:
+ *                                user:
+ *                                  $ref: "#/components/schemas/UserEntity"
+ *          400:
+ *              description: |
+ *                오류가 발생해 특정 user를 subject 랜덤 매칭 대기열에 추가하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
+ *                -오류 예시</br>
+ *                - 존재하지 않는 userID가 들어온 경우 <br>
+ *                - No user in DB <br>
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                            success:
+ *                              type: boolean
+ *                            error:
+ *                              properties:
+ *                               code:
+ *                                 type: string
+ *                               message:
+ *                                 type: string
+ *                          example:
+ *                            success: false
+ *                            error:
+ *                              code: Error
+ *                              message: This userID does not exist.
+ *          401:
+ *              description: Authentication Error |
+ *                user인증에 실패했을 경우 해당 오류가 반환됩니다.</br></br>
+ *                -오류 예시</br>
+ *                - access_token or platform을 헤더값에 넣지 않고 요청을 보낸경우 <br>
+ *                - 만료되거나 올바르지 않은 형식의 access_token을 헤더에 넣어 보낸 경우 <br>
+ *                - platform값이 naver, kakao를 제외한 값인 경우
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                            success:
+ *                              type: boolean
+ *                            error:
+ *                              properties:
+ *                               code:
+ *                                 type: string
+ *                               message:
+ *                                 type: string
+ *                          example:
+ *                            success: false
+ *                            error:
+ *                              code: authentication
+ *                              message: The value of headers.access_token is not valid.
  */
