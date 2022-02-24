221d3d2e6f96cc568c97609ca9f71b22cf10bb77
Lks9172 <ekdan9172@naver.com>
2022-02-24 10:00:18 +0900

    docs: get /notice, post/question 에 대한 api 문서작성

---
+++ srcs/swagger/schema/notice.ts
@@ -0,0 +1,20 @@
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Notice:
+ *              type: object
+ *              properties:
+ *                  id:
+ *                      type: string
+ *                  title:
+ *                      type: string
+ *                  content:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *                  created_at:
+ *                      type: string
+ *                  updated_at:
+ *                      type: string
+ */
