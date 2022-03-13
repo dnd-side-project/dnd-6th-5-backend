9ffd0e9daf26c17bc9c3294c3eac297eb5d0a907
Lks9172 <ekdan9172@naver.com>
2022-03-13 21:26:22 +0900

    env: 통합테스트를 위해 supertest 실행환경 구성
    - jset.config.js 설정중 transform에서 js확장자도 추가
    - ormconfig.js에서 test DB설정 추가
    - package.json supertest 패키지 명시
    - tsconfig.json에서 js확장자 파일인 jest설정파일을 읽을 수 있도록 allowJs 설정 true로 설정

--- ormconfig.js
+++ ormconfig.js
@@ -2,17 +2,35 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = {
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    dropSchema: false,
-    entities: ['./srcs/entity/*.ts'],
-    subscribers: ['srcs/migration/*.ts'],
-    migrations: ['srcs/migration/*.ts'],
-};
+module.exports = [
+    {
+        name: 'default',
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        logging: false,
+        dropSchema: false,
+        entities: ['./srcs/entity/*.ts'],
+        subscribers: ['srcs/migration/*.ts'],
+        migrations: ['srcs/migration/*.ts'],
+    },
+    {
+        name: 'test',
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        logging: false,
+        dropSchema: false,
+        entities: ['./srcs/entity/*.ts'],
+        subscribers: ['srcs/migration/*.ts'],
+        migrations: ['srcs/migration/*.ts'],
+    },
+]
