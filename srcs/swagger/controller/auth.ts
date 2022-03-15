851b578d123322a074053053c6235daf7f1c3b40
Lks9172 <ekdan9172@naver.com>
2022-03-16 00:14:53 +0900

    feat: 소셜 계정 탈퇴 api 문서 작성 완료
    - delete /user/kakao 에 대한 api문서 작성 완료
    - delete /user/naver 에 대한 api문서 작성 완료

--- srcs/swagger/controller/auth.ts
+++ srcs/swagger/controller/auth.ts
@@ -208,7 +208,7 @@
  *                                      type: number
  *              400:
  *                  description: |
- *                    오류가 발생해 spoon feed에 naver 계정으로 성공적으로 소셜 로그인하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
+ *                    오류가 발생해 spoon feed에 naver 계정으로 성공적으로 소셜 로그아웃하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
  *                    -오류 예시</br>
  *                    - header에 access_token 넣지 않고 요청했을 경우 <br>
  *                  content:
@@ -286,7 +286,7 @@
  *                                      type: number
  *              400:
  *                  description: |
- *                    오류가 발생해 spoon feed에 kakao 계정으로 성공적으로 소셜 로그인하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
+ *                    오류가 발생해 spoon feed에 kakao 계정으로 성공적으로 소셜 로그아웃하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
  *                    -오류 예시</br>
  *                    - header에 access_token 넣지 않고 요청했을 경우 <br>
  *                  content:
@@ -310,6 +310,162 @@
  *              401:
  *                  description: |
  *                    오류가 발생해 spoon feed에 kakao 계정으로 성공적으로 소셜 로그아웃하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
+ *                    -오류 예시</br>
+ *                    - 만료되거나 올바르지않은 access_token이 요청 헤더에 들어온 경우<br>
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                success:
+ *                                  type: boolean
+ *                                error:
+ *                                  properties:
+ *                                   code:
+ *                                     type: string
+ *                                   message:
+ *                                     type: string
+ *                              example:
+ *                                success: false
+ *                                error:
+ *                                  error: invalid_grant
+ *                                  error_description: expired_or_invalid_refresh_token
+ *                                  error_code: KOE322
+ */
+
+/**
+ * @swagger
+ * paths:
+ *  /user/naver:
+ *      delete:
+ *          description: spoon feed에 네이버 계정으로 탈퇴 합니다.
+ *          tags: [Auth]
+ *          parameters:
+ *              - in: header
+ *                name: access_token
+ *                required: true
+ *                description: naver api에서 발급받은 access_token
+ *                schema:
+ *                    type: string
+ *          responses:
+ *              200:
+ *                  description: spoon feed에 naver 계정으로 성공적으로 탈퇴하였을 경우 다음 결과가 반환됩니다. <br>
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                success:
+ *                                  type: boolean
+ *                                data:
+ *                                  type: object
+ *                                  properties:
+ *                                    id:
+ *                                      type: number
+ *              400:
+ *                  description: |
+ *                    오류가 발생해 spoon feed에 naver 계정으로 성공적으로 탈퇴하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
+ *                    -오류 예시</br>
+ *                    - header에 access_token 넣지 않고 요청했을 경우 <br>
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                success:
+ *                                  type: boolean
+ *                                error:
+ *                                  properties:
+ *                                   code:
+ *                                     type: string
+ *                                   message:
+ *                                     type: string
+ *                              example:
+ *                                errors:
+ *                                  msg: Invalid value
+ *                                  param: access_token
+ *                                  location: headers
+ *              401:
+ *                  description: |
+ *                    인증 오류가 발생해 spoon feed에 naver 계정으로 성공적으로 탈퇴하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
+ *                    -오류 예시</br>
+ *                    - 만료되거나 올바르지않은 access_token이 요청 헤더에 들어온 경우<br>
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                success:
+ *                                  type: boolean
+ *                                error:
+ *                                  properties:
+ *                                   code:
+ *                                     type: string
+ *                                   message:
+ *                                     type: string
+ *                              example:
+ *                                success: false
+ *                                error:
+ *                                  error: invalid_grant
+ *                                  error_description: expired_or_invalid_refresh_token
+ *                                  error_code: KOE322
+ */
+
+/**
+ * @swagger
+ * paths:
+ *  /user/kakao:
+ *      delete:
+ *          description: spoon feed에 가입된 kakao 계정을 탈퇴합니다.
+ *          tags: [Auth]
+ *          parameters:
+ *              - in: header
+ *                name: access_token
+ *                required: true
+ *                description: kakao api에서 발급받은 access_token
+ *                schema:
+ *                    type: string
+ *          responses:
+ *              200:
+ *                  description: spoon feed에 kakao 계정으로 성공적으로 탈퇴하였을 경우 다음 결과가 반환됩니다. <br>
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                success:
+ *                                  type: boolean
+ *                                data:
+ *                                  type: object
+ *                                  properties:
+ *                                    id:
+ *                                      type: number
+ *              400:
+ *                  description: |
+ *                    오류가 발생해 spoon feed에 kakao 계정으로 성공적으로 탈퇴하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
+ *                    -오류 예시</br>
+ *                    - header에 access_token 넣지 않고 요청했을 경우 <br>
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                success:
+ *                                  type: boolean
+ *                                error:
+ *                                  properties:
+ *                                   code:
+ *                                     type: string
+ *                                   message:
+ *                                     type: string
+ *                              example:
+ *                                errors:
+ *                                  msg: Invalid value
+ *                                  param: access_token
+ *                                  location: headers
+ *              401:
+ *                  description: |
+ *                    오류가 발생해 spoon feed에 kakao 계정으로 성공적으로 소셜 계정을 탈퇴하지 못했을 경우 다음 결과가 반환됩니다.</br></br>
  *                    -오류 예시</br>
  *                    - 만료되거나 올바르지않은 access_token이 요청 헤더에 들어온 경우<br>
  *                  content:
