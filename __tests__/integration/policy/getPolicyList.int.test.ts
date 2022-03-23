bd8f53015e4268913eff27ad5a6d619085cc4fda
yewon <yewon@JongSukui-MacBook.local>
2022-03-24 02:30:26 +0900

    test: get /policy 통합 테스트 코드

---
+++ __tests__/integration/policy/getPolicyList.int.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { expect, it, beforeAll } from '@jest/globals';
+import createServer from '../../../srcs/app';
+
+import category from '../../data/category.json';
+import result from '../../data/policy.json';
+import error from '../../data/error.json';
+
+let app;
+beforeAll(async () => {
+    app = await createServer();
+});
+describe('GET /policy', () => {
+    it('200: normal request', async () => {
+        const response = await request(app).get('/policy').query(category);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.success).toBe(true);
+        // expect(response.body.data.policy).toStrictEqual(result);  // 테스트 디비와 운영 디비 싱크 맞춘 후 다시
+    });
+
+    it('400: invalid category error', async () => {
+        const query = {
+            category: 'invalid!!'
+        };
+        const response = await request(app).get('/policy').query(query);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toStrictEqual(error.invalidCategory);
+    });
+
+});
