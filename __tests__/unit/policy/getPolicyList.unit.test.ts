b2414d06643e60af5c28901082051e053b23cff0
yewon <yewon@JongSukui-MacBook.local>
2022-03-23 18:43:36 +0900

    test: get /policy 유닛 테스트

--- __tests__/unit/policy/getPolicyList.unit.test.ts
+++ __tests__/unit/policy/getPolicyList.unit.test.ts
@@ -5,6 +5,7 @@ import { Policy } from '../../../srcs/entity/index';
 
 import tJest from '../../../@types/types';
 import policy from '../../data/policy.json';
+import category from '../../data/category.json';
 
 jest.mock('../../../srcs/entity/index');
 
@@ -28,6 +29,7 @@ beforeEach(async () => {
 
 describe('getPolicyList Controller GET', () => {
     beforeEach(async () => {
+        req.query = category;
         await mockPolicyEntity.createQueryBuilder.mockClear();
     });
 
@@ -41,6 +43,34 @@ describe('getPolicyList Controller GET', () => {
         await getPolicyList(req, res, next);
 
         expect(res.statusCode).toBe(200);
-        // expect(res._isEndCalled()).toBeTruthy();
+        expect(res._isEndCalled()).toBeTruthy();
+    });
+
+    it('should return json body in response', async () => {
+        const resJsonData = {
+            success: true,
+            data: { policy: policy },
+        };
+
+        await getPolicyList(req, res, next);
+
+        expect(res._getJSONData()).toStrictEqual(resJsonData);
+    });
+
+    it('should call createQueryBuilder', async () => {
+        getPolicyList(req, res, next);
+
+        // Policy.createQueryBuilder()가 1번 실행되었는지 확인합니다.
+        expect(res.statusCode).toBe(200);
+        expect(await mockPolicyEntity.createQueryBuilder).toBeCalledTimes(1);
+    });
+
+    // invalid category 값이 req.query에 들어올 경우 에러처리 test code
+    it('should handle invalid category error', async () => {
+        req.query.category = 'invalid category';
+        const error = Error('Invalid Category');
+
+        await getPolicyList(req, res, next);
+        expect(next).toBeCalledWith(error);
     });
 });
