506559b7593fa7ee7978eb22c404fb3282a35cd7
Lks9172 <ekdan9172@naver.com>
2022-04-03 23:11:17 +0900

    feature: request를 보낸 클라이언트의 protocol, host의 정보 로그남기는 미들웨어 구현 - originLogger

--- srcs/middleware/index.ts
+++ srcs/middleware/index.ts
@@ -1,2 +1,3 @@
 export { default as isAuth } from './middleware.isAuth';
 export { default as validator } from './middleware.validator';
+export { default as originLogger } from './middleware.originLogger';
